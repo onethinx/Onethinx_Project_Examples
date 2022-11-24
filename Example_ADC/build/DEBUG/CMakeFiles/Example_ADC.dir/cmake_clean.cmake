67747c20df9448b243e34a6e15a247e418a09bbe
RolfNoot <smits@wth.nl>
2022-11-24 16:49:37 +0100

    updated gitignore

--- Example_ADC/build/DEBUG/CMakeFiles/Example_ADC.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Example_ADC"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Example_ADC.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Example_ADC/build/DEBUG/CMakeFiles/Example_ADC.elf.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Example_ADC.elf.
-# This may be replaced when dependencies are built.
