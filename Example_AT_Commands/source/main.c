4ce46fa6e72b776bd125ffab2c79a27de0ff391f
RolfNoot <smits@wth.nl>
2022-11-26 22:21:50 +0100

    removed delay from main as it's inserted by corestripper

--- Example_AT_Commands/source/main.c
+++ Example_AT_Commands/source/main.c
@@ -7,7 +7,7 @@
  *
  ********************************************************************************
  *
- * Copyright (c) 2019 Onethinx BV <info@onethinx.com>
+ * Copyright (c) 2019-2022 Onethinx BV <info@onethinx.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
